224950dec6b5c3c132859509d4778bd15ddc6291
zaidsmd <zaidsmd111@gmail.com>
2022-11-27 23:33:59 +0100

    firefox has selector fix

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var inputs = document.querySelectorAll("input:not([type=radio],select)");
-var nameKey, brandKey, dateKey, selectKey, priceKey;
-var select = document.querySelector('select');
+let inputs = document.querySelectorAll("input:not([type=radio],select)");
+let nameKey, brandKey, dateKey, selectKey, priceKey;
+let select = document.querySelector('select');
 let add = document.getElementById('add');
-var table = document.getElementById('table');
+let table = document.getElementById('table');
+let dataToModify = []
 inputs.forEach(element => {
     element.addEventListener('keyup', () => {
         if (element.name === 'product-name') {
@@ -56,27 +57,67 @@ add.addEventListener('click', () => {
 
     } else {
         if (selectKey && dateKey && nameKey && brandKey && priceKey) {
-            let productName = document.querySelector("[name='product-name']").value;
-            let brand = document.querySelector("[name='brand']").value;
-            let price = Number(document.querySelector('[name=price]').value).toFixed(2);
-            let date = document.querySelector('[name=date]').value;
-            let category = document.querySelector('option:checked').innerHTML;
-            let sale = document.querySelector('label:has([type="radio"]:checked) span').innerHTML;
+            let productName = document.querySelector("[name='product-name']").value.trim();
+            let brand = document.querySelector("[name='brand']").value.trim();
+            let price = '$ ' + Number(document.querySelector('[name=price]').value).toFixed(2);
+            let date = document.querySelector('[name=date]').value.trim();
+            let category = document.querySelector('option:checked').innerHTML.trim();
+            let sale = document.querySelector('[type="radio"]:checked ').value;
             let row = document.createElement('div');
-            buttons = '';
             row.classList.add('row');
-            let data = [productName,brand,price,date,category,sale,buttons];
-            for (let i = 0; i <data.length ; i++) {
-                var column = document.createElement('div');
+            let modifyBtn = document.createElement('button');
+            modifyBtn.innerHTML = 'Modify';
+            modifyBtn.classList.add('primary-btn');
+            modifyBtn.id = 'modify';
+            let deleteBtn = document.createElement('button');
+            deleteBtn.innerHTML = 'Delete';
+            deleteBtn.classList.add('secondary-btn');
+            deleteBtn.id = 'delete';
+            let lastColumn = document.createElement('div');
+            lastColumn.classList.add('column');
+            lastColumn.appendChild(deleteBtn);
+            lastColumn.appendChild(modifyBtn);
+            let data = [productName, brand, price, date, category, sale];
+            for (let i = 0; i < data.length; i++) {
+                let column = document.createElement('div');
                 column.classList.add('column');
-                var value = document.createTextNode(data[i]);
+                let value = document.createTextNode(data[i]);
                 column.appendChild(value);
                 row.appendChild(column);
             }
-            row.lastChild.innerHtml = `<button id="delete" class="secondary-btn">Delete</button>
-                    <button id="modify" class="primary-btn">Modify</button>`;
+            row.appendChild(lastColumn)
             table.appendChild(row);
+            document.querySelector('form').reset()
+            inputs.forEach(e=>{
+                e.value = '';
+                e.setAttribute('value','')
+                e.classList.remove('valid')
+            })
+            document.querySelector('select').classList.remove('valid')
+            document.querySelectorAll('#modify').forEach(e=>{
+                e.addEventListener('click',event=>{
+                    Object.values(e.parentElement.parentElement.children).forEach((element,index)=>{
+                        if (index===2){
+                           dataToModify.push(Number(element.innerHTML.replace(/^\D+/g,'').trim()))
+                        }else if(index<5){
+                            dataToModify.push(element.innerHTML);
+                        }
+                    })
+                    dataToModify.forEach((el,i)=>{
+                        if(i<4){
+                            inputs[i].value = el;
+                        }
+                    })
+                    document.querySelector(`[value=${dataToModify[4]}]`).setAttribute('checked','');
+                    if (dataToModify[5]==='yes'){
+                        document.getElementById('on-sale').setAttribute('checked','');
+                    }else {
+                        document.getElementById('not-on-sale').setAttribute('checked','');
+                    }
+
+                })
+            })
+
         }
     }
 })
-
