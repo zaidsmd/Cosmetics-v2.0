6c62d5a260c824993a7aa64e29df3d4825a6bdaf
zaidsmd <zaidsmd111@gmail.com>
2022-12-12 08:09:36 +0100

    stting

--- script.js
+++ script.js
@@ -1,189 +1,85 @@
-let inputs = document.querySelectorAll("input:not([type=radio],select)");
-let nameKey, brandKey, dateKey, selectKey, priceKey;
-let select = document.querySelector('select');
+let name = document.getElementById('product-name');
+let brand = document.getElementById('brand');
+let price = document.getElementById('price');
+let date = document.getElementById('date');
+let category = document.getElementById('category');
+let sale = document.querySelector('[name=sale]:checked');
 let add = document.getElementById('add');
-let table = document.getElementById('table');
-let dataToModify = [];
-let toDelete;
-let selectedRow;
-add.addEventListener('click', () => {
-    validateInput();
-    if (document.querySelector('input:checked') === null) {
-    } else {
-        if (selectKey && dateKey && nameKey && brandKey && priceKey) {
-            let productName = document.querySelector("[name='product-name']").value.trim();
-            let brand = document.querySelector("[name='brand']").value.trim();
-            let price = '$ ' + Number(document.querySelector('[name=price]').value).toFixed(2);
-            let date = document.querySelector('[name=date]').value.trim();
-            let category = document.querySelector('option:checked').innerHTML.trim();
-            let sale = document.querySelector('[type="radio"]:checked ').value;
-            let row = document.createElement('div');
-            row.classList.add('row');
-            let modifyBtn = document.createElement('button');
-            modifyBtn.innerHTML = 'Modify';
-            modifyBtn.classList.add('primary-btn');
-            modifyBtn.classList.add('modify')
-            let deleteBtn = document.createElement('button');
-            deleteBtn.innerHTML = 'Delete';
-            deleteBtn.classList.add('secondary-btn');
-            deleteBtn.classList.add('delete');
-            let lastColumn = document.createElement('div');
-            lastColumn.classList.add('column');
-            lastColumn.appendChild(deleteBtn);
-            lastColumn.appendChild(modifyBtn);
-            let data = [productName, brand, price, date, category, sale];
-            for (let i = 0; i < data.length; i++) {
-                let column = document.createElement('div');
-                column.classList.add('column');
-                let value = document.createTextNode(data[i]);
-                column.appendChild(value);
-                row.appendChild(column);
-            }
-            row.appendChild(lastColumn)
-            table.appendChild(row);
-            document.querySelector('form').reset()
-            inputs.forEach(e => {
-                e.value = '';
-                e.setAttribute('value', '')
-                e.classList.remove('valid')
-            })
-            document.querySelector('select').classList.remove('valid');
-            document.querySelector('[type=radio]')
-            document.querySelectorAll('.modify').forEach(e => {
-                e.addEventListener('click', () => {
-                    dataToModify = [];
-                    document.querySelector('form').reset()
-                    inputs.forEach(function () {
-                        e.value = ''
-                    })
-                    selectedRow = Object.values(e.parentElement.parentElement.children);
-                    selectedRow.forEach((element, index) => {
-                        if (index === 2) {
-                            dataToModify.push(Number(element.innerHTML.replace(/^\D+/g, '').trim()))
-                        } else if (index <= 5) {
-                            dataToModify.push(element.innerHTML);
-                        }
-                    })
-                    dataToModify.forEach((el, i) => {
-                        if (i < 4) {
-                            inputs[i].value = el;
-                        }
-                    })
-                    document.querySelector(`select`).value = dataToModify[4];
-                    if (dataToModify[5] === 'Yes') {
-                        document.getElementById('on-sale').setAttribute('checked', '');
-                    } else {
-                        document.getElementById('not-on-sale').setAttribute('checked', '');
-                    }
-                    inputs.forEach(e => {
-                        e.setAttribute('value', e.value);
-                    })
-                    add.style.display = 'none';
-                    document.getElementById('update').style.display = 'block';
-                    document.getElementById('update').addEventListener('click', function () {
-                        validateInput();
-                        if (document.querySelector('input:checked') === null) {
-                        } else {
-                            if (selectKey && dateKey && nameKey && brandKey && priceKey) {
-                                productName = document.querySelector("[name='product-name']").value.trim();
-                                brand = document.querySelector("[name='brand']").value.trim();
-                                price = '$ ' + Number(document.querySelector('[name=price]').value).toFixed(2);
-                                date = document.querySelector('[name=date]').value.trim();
-                                category = document.querySelector('option:checked').innerHTML.trim();
-                                sale = document.querySelector('[type="radio"]:checked ').value;
-                                let updatedData = [productName, brand, price, date, category, sale]
-                                updatedData.forEach((e, i) => {
-                                    selectedRow[i].innerHTML = e;
-                                })
-                                add.style.display = 'block';
-                                document.getElementById('update').style.display = 'none'
-                                document.querySelector('form').reset()
-                                inputs.forEach(e => {
-                                    e.value = '';
-                                    e.setAttribute('value', '')
-                                    e.classList.remove('valid')
-                                })
-                                document.querySelector('[type=radio]:checked').removeAttribute('checked')
-                                document.querySelector('select').classList.remove('valid');
-                            }
-                        }
-
-                    })
-                })
-                document.querySelectorAll('.delete').forEach(e => {
-                    e.addEventListener('click', function (e) {
-                        document.querySelector('dialog').showModal();
-                        toDelete = e.target.parentElement.parentElement;
-                    })
-                })
-
-            })
-
-        }
+let table = [];
+class Product {
+    constructor(name,brand,price,date,category,sale) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.date = date;
+        this.category = category;
+        this.sale=sale;
     }
-})
-document.getElementById('modal-delete').addEventListener('click', () => {
-    toDelete.remove();
-    document.querySelector('dialog').close();
-})
-document.getElementById('cancel').addEventListener('click', () => {
-    document.querySelector('dialog').close();
-})
-
-function validateInput() {
-    inputs.forEach(element => {
-        if (element.name === 'product-name') {
-            if (element.value === '' && element.value.length <= 30) {
-                nameKey = false;
-                element.classList.remove('valid')
-                element.classList.add('invalid')
-            } else {
-                element.classList.remove('invalid')
-                element.classList.add('valid')
-                nameKey = true;
-            }
-        }
-        if (element.name === 'brand') {
-            if (element.value === '') {
-                brandKey = false;
-                element.classList.remove('valid')
-                element.classList.add('invalid')
-            } else {
-                element.classList.add('valid')
-                element.classList.remove('invalid')
-                brandKey = true;
-            }
-        }
-        if (element.name === 'date') {
-            if (element.value === '') {
-                dateKey = false;
-                element.classList.remove('valid');
-                element.classList.add('invalid');
-            } else {
-                element.classList.add('valid');
-                element.classList.remove('invalid');
-                dateKey = true;
-            }
-        }
-        if (element.name === 'price') {
-            if (element.value === '' || Number(element.value) < 0) {
-                priceKey = false;
-                element.classList.remove('valid');
-                element.classList.add('invalid');
-            } else {
-                element.classList.add('valid');
-                element.classList.remove('invalid');
-                priceKey = true;
-            }
-        }
-    })
-    if (select.value === '0') {
-        selectKey = false;
-        select.classList.add('invalid');
-        select.classList.remove('valid');
-    } else {
-        selectKey = true;
-        select.classList.remove('invalid');
-        select.classList.add('valid');
+    details() {
+        return `<p>Product Details</p>
+                <p>Name: ${this.name}</p>
+                <p>Brand: ${this.brand}</p>
+                <p>Price: ${this.price} DH</p>
+                <p>Date: ${this.date}</p>
+                <p>Category: ${this.category}</p>
+                <p>On sale: ${this.sale}</p>`
     }
 }
+if (window.localStorage.table === undefined) {
+    window.localStorage.setItem('table','');
+}else if (window.localStorage.table !== ''){
+    table = JSON.parse(window.localStorage.table);
+}
+function validation(){
+    let key = true;
+    if (!(/^[a-z]{1,30}$/i).test(name.value)){
+        key = false;
+        name.classList.add('invalid');
+        name.classList.remove('valid');
+    }else {
+        name.classList.add('valid');
+        name.classList.remove('invalid');
+    }
+    if (brand.value === ''){
+        key =false;
+        brand.classList.add('invalid');
+        brand.classList.remove('valid');
+    }else {
+        brand.classList.add('valid');
+        brand.classList.remove('invalid')
+    }
+    if (!(/\d+/).test(price.value)){
+        key = false;
+        price.classList.add('invalid');
+        price.classList.remove('valid');
+    }else {
+        price.classList.add('valid');
+        price.classList.remove('invalid');
+    }
+    if (date.value === ''){
+        key = false;
+        date.classList.add('invalid');
+        date.classList.remove('valid');
+    }else {
+        date.classList.add('valid');
+        date.classList.remove('invalid');
+    }
+    if (category.value === '0'){
+        key = false;
+        category.classList.add('invalid');
+        category.classList.remove('valid');
+    }else {
+        category.classList.add('valid');
+        category.classList.remove('invalid');
+    }
+    if (sale=== null) {
+        key = false ;
+        document.querySelector('.forRadio').classList.add('invalid');
+        document.querySelector('.forRadio').classList.remove('valid');
+    }else {
+        document.querySelector('.forRadio').classList.add('valid');
+        document.querySelector('.forRadio').classList.remove('invalid');
+    }
+}
+
+
